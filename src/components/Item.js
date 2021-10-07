2e7c824290274ca184ede5560bbe42e294102a28
Wilfred <wilfredbayudan@gmail.com>
2021-10-06 21:58:35 -1000

    commit tree 83a507da74df4fbf870ade368282078444b98ed3

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,54 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
+class Item extends React.Component {
 
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+  state = {
+    isInCart: false
   }
 
-  return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-    </li>
-  );
+  handleAddToCartClick = () => {
+    this.setState({
+      isInCart: !this.state.isInCart
+    })
+  }
+
+  render() {
+
+    return (
+      <li className={this.state.isInCart ? "in-cart" : ""}>
+        <span>{this.props.name}</span>
+        <span className="category">{this.props.category}</span>
+        <button
+          className={this.state.isInCart ? "remove" : "add"}
+          onClick={this.handleAddToCartClick}
+        >
+          {this.state.isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+      </li>
+    );
+  }
+
 }
+
+// function Item({ name, category }) {
+//   const [isInCart, setIsInCart] = useState(false);
+
+//   function handleAddToCartClick() {
+//     setIsInCart((isInCart) => !isInCart);
+//   }
+
+//   return (
+//     <li className={isInCart ? "in-cart" : ""}>
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button
+//         className={isInCart ? "remove" : "add"}
+//         onClick={handleAddToCartClick}
+//       >
+//         {isInCart ? "Remove From" : "Add to"} Cart
+//       </button>
+//     </li>
+//   );
+// }
 
 export default Item;
